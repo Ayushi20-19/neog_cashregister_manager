243cf739b2ddb2fe6cc3327657a5d2c426eb2482
Ayushi Verma <50084909+Ayushi20-19@users.noreply.github.com>
2021-08-23 11:21:25 +0530

    css done

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-import "./styles.css";
-
 var billAmount = document.querySelector(".billAmount");
 var btnBillCheck = document.querySelector(".btnBillCheck");
 var msgDisplay1 = document.querySelector("#msg1");
@@ -8,11 +6,11 @@ var btnGivenAmountCheck = document.querySelector(".btnGivenAmountCheck");
 var msgDisplay2 = document.querySelector("#msg2");
 var display2 = document.querySelector("#display2");
 var showNotes = document.querySelectorAll(".showNotes");
-var output = document.querySelector(".output")
+var output = document.querySelector(".output");
 amountGiven.style.display = "none";
 btnGivenAmountCheck.style.display = "none";
 display2.style.display = "none";
-output.style.display ="none";
+output.style.display = "none";
 var arrNote = [2000, 500, 100, 50, 20, 10, 5, 2, 1];
 //showNotes =[]
 const billCheckClickHandler = () => {
@@ -40,17 +38,19 @@ const givenCashHandler = () => {
     var amountToBeReturn = amountGiven.value - billAmount.value;
     returnCash(amountToBeReturn);
     msg = "cash notes";
-    output.style.display ="block";
   } else if (amountGiven.value === billAmount.value) {
     msg = "you donot need to return anything";
+    output.style.display = "none";
   } else {
     msg = "enter valid amount";
+    output.style.display = "none";
   }
   msgDisplay2.innerHTML = msg;
 };
 
 const returnCash = (amountToBeReturn1) => {
   console.log(amountToBeReturn1);
+  output.style.display = "block";
   for (var i = 0; i < arrNote.length; i++) {
     const left = Math.trunc(amountToBeReturn1 / arrNote[i]);
     console.log(left);
